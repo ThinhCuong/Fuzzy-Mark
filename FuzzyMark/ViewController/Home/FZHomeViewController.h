240c40b7b4a285a283027088b55e409d95384c28
cuongnt <cuongnt@ttc-solutions.com>
2019-04-14 20:06:30 +0700

    custom appDelegate

--- FuzzyMark/ViewController/Home/FZHomeViewController.h
+++ FuzzyMark/ViewController/Home/FZHomeViewController.h
@@ -6,11 +6,11 @@
 //  Copyright © 2019 Nguyen Cuong. All rights reserved.
 //
 
-#import "ViewController.h"
+#import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface FZHomeViewController : ViewController
+@interface FZHomeViewController : UIViewController
 
 @end
 
