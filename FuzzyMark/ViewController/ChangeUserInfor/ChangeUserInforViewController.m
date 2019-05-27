2e8954d182307ca1ea2e368b2162a0723db006dd
Tu Tran <tutv@ttc-solutions.com>
2019-05-28 02:21:22 +0700

    dung check box male female.

--- FuzzyMark/ViewController/ChangeUserInfor/ChangeUserInforViewController.m
+++ FuzzyMark/ViewController/ChangeUserInfor/ChangeUserInforViewController.m
@@ -8,11 +8,14 @@
 
 #import "ChangeUserInforViewController.h"
 #import "FuzzyMark-Swift.h"
+#import <TNCheckBoxGroup/TNCheckBoxGroup.h>
 
 @interface ChangeUserInforViewController ()
 @property (weak, nonatomic) IBOutlet TJTextField *txtName;
 @property (weak, nonatomic) IBOutlet TJTextField *txtBirthday;
 @property (weak, nonatomic) IBOutlet UIButton *btnSave;
+@property (weak, nonatomic) IBOutlet UIView *checkBoxView;
+
 
 @end
 
@@ -27,7 +30,36 @@
 }
 
 - (void)setUI {
-  
+    TNCircularCheckBoxData *maleData = [[TNCircularCheckBoxData alloc] init];
+    maleData.identifier = @"male";
+    maleData.labelText = @"Nam";
+    maleData.labelFont = [UIFont setFontMuliWithSize:14];
+    maleData.checked = YES;
+    maleData.borderColor = [UIColor colorWithRed:0.2 green:0.6 blue:0.86 alpha:1.0];
+    maleData.circleColor = [UIColor colorWithRed:0.2 green:0.6 blue:0.86 alpha:1.0];
+    maleData.borderRadius = 20;
+    maleData.circleRadius = 15;
+    
+    TNCircularCheckBoxData *femaleData = [[TNCircularCheckBoxData alloc] init];
+    femaleData.identifier = @"male";
+    femaleData.labelText = @"Nữ";
+    femaleData.labelFont = [UIFont setFontMuliWithSize:14];
+    femaleData.checked = NO;
+    femaleData.borderColor = [UIColor colorWithRed:0.2 green:0.6 blue:0.86 alpha:1.0];
+    femaleData.circleColor = [UIColor colorWithRed:0.2 green:0.6 blue:0.86 alpha:1.0];
+    femaleData.borderRadius = 20;
+    femaleData.circleRadius = 15;
+    
+    TNCheckBoxGroup *sex = [[TNCheckBoxGroup alloc] initWithCheckBoxData:@[maleData, femaleData] style:TNCheckBoxLayoutHorizontal];
+    [sex create];
+    sex.position = CGPointMake(0, 0);
+    
+    [self.checkBoxView addSubview:sex];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(sexGroupChanged:) name:GROUP_CHANGED object:sex];
+}
+
+- (void)sexGroupChanged:(NSNotification *) notication {
+    
 }
 
 - (void)setNavigationBar {
