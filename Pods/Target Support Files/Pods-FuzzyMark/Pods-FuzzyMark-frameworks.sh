734cd2723c261e6ad3332d1eac43d054ca3300ba
Tu Tran <tutv@ttc-solutions.com>
2019-06-01 10:52:22 +0700

    add lại frameword Toast

--- Pods/Target Support Files/Pods-FuzzyMark/Pods-FuzzyMark-frameworks.sh
+++ Pods/Target Support Files/Pods-FuzzyMark/Pods-FuzzyMark-frameworks.sh
@@ -160,6 +160,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TNCheckBoxGroup/TNCheckBoxGroup.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Toast/Toast.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CCBottomRefreshControl/CCBottomRefreshControl.framework"
@@ -169,6 +170,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SVProgressHUD/SVProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TNCheckBoxGroup/TNCheckBoxGroup.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Toast/Toast.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
