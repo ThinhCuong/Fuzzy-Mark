9d974413ad836be21bfe0157c859a8bd87729c85
cuongnt <cuongnt@ttc-solutions.com>
2019-06-17 17:56:20 +0700

    commit xib

---
+++ FuzzyMark/ViewController/VouchersSearch/FZVourchersSearchViewController.h
@@ -0,0 +1,19 @@
+//
+//  FZVourchersSearchViewController.h
+//  FuzzyMark
+//
+//  Created by Nguyen Cuong on 6/13/19.
+//  Copyright © 2019 Nguyen Cuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FZVourchersSearchViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FuzzyMark/ViewController/VouchersSearch/FZVourchersSearchViewController.m
@@ -0,0 +1,64 @@
+//
+//  FZVourchersSearchViewController.m
+//  FuzzyMark
+//
+//  Created by Nguyen Cuong on 6/13/19.
+//  Copyright © 2019 Nguyen Cuong. All rights reserved.
+//
+
+#import "FZVourchersSearchViewController.h"
+#import "FZVourcherSearchTableViewCell.h"
+#import "FZHomeModel.h"
+
+@interface FZVourchersSearchViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) NSArray <FZGroupInfoJsonModel> *listVourcher;
+
+@end
+
+@implementation FZVourchersSearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    [self.tableView registerNib:[UINib nibWithNibName:@"FZVourcherSearchTableViewCell" bundle:nil] forCellReuseIdentifier:@"FZVourcherSearchTableViewCell"];
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    [self callVouchersSearch];
+}
+
+
+- (IBAction)backToParentViewController:(UIButton *)sender {
+    [self.navigationController popViewControllerAnimated:YES];
+}
+
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.listVourcher.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    FZVourcherSearchTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FZVourcherSearchTableViewCell"];
+    [cell bindData:self.listVourcher[indexPath.row]];
+    return cell;
+}
+
+- (void)callVouchersSearch {
+    
+    NSDictionary *params = @{
+                             @"token": @"abd",
+                             };
+    
+    [SVProgressHUD setContainerView:self.view];
+    [SVProgressHUD show];
+    [[BaseCallApi defaultInitWithBaseURL] getDataWithPath:@"vouchers/search" andParam:params isShowfailureAlert:YES withSuccessBlock:^(id responseData) {
+        [SVProgressHUD dismiss];
+        if (responseData) {
+            FzVourcherSearch *GroupInfoData = [[FzVourcherSearch alloc] initWithDictionary:responseData error:nil];
+            self.listVourcher = GroupInfoData.data;
+            [self.tableView reloadData];
+        }
+    } withFailBlock:^(id responseError) {
+        [SVProgressHUD dismiss];
+    }];
+}
+
+@end
